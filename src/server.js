cf247cc4a8d595a61e7323e89c046de40f0e3079
Stanislave <stasdroner@gmail.com>
2026-08-17 18:21:13 +0300

    Fix port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import pino from 'pino-http';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
