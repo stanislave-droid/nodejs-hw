50efed8896634ce5635ca0df200441e2cc9dae58
Stanislave <stasdroner@gmail.com>
2026-08-17 18:34:06 +0300

    Fix error display

--- src/server.js
+++ src/server.js
@@ -46,7 +46,6 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   console.error(err.message);
-  console.log(process.env);
   res.status(500).json({
     message: 'Internal server error',
     error:
